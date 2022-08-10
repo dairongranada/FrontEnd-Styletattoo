6fb36573c8212f66780bb7cf28773cf5310c74e5
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-08-10 11:38:17 -0500

    se corrigieron errores -JD

--- src/components/Layouts/LayoutsQuotes/ContQuotes/ContQuotes.jsx
+++ src/components/Layouts/LayoutsQuotes/ContQuotes/ContQuotes.jsx
@@ -6,17 +6,17 @@ export const ContQuotes = () => {
     <div className='backgruondCont'>
         <div className='ShadowCont'>
             <form className='FormQuot'>
-                <h3>Intruce la Infromacion</h3>
+                <h3>Introduce la Informacion</h3>
                 <p>Fecha</p>
                 <input className='input_global_style' type="text" required />
                 <br />
-                <h3>hora</h3>
+                <h3>Hora</h3>
                 <input type="time"/>
                 <div className='buttons_global_StyleTatto' id='addfile'>
                     <p>Muestranos tu idea</p>
                     <input id='btnEnviar' type="file" required/>
                 </div>
-                <p>dejanos un mensaje</p>
+                <p>Dejanos un mensaje</p>
                 <textarea name="" id="" cols="48" rows="6"></textarea>
                 <button className='buttons_global_StyleTatto'>Enviar</button>
             </form>

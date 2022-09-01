5dcf1bb3c19af86f1279044f0900b219055fca43
Dairon Granada <93607018+dairongranada@users.noreply.github.com>
2022-09-01 07:33:47 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import { Page404 } from './components/Pages/Page404/Page404';
 import { PageFormSe } from './components/Pages/PageFormSe/PageFormSe';
 
 
+import { PagePiercings } from './components/Pages/___PagePiercings/PagePiercings';
+
+
+
 
 
 function App() {
@@ -28,8 +32,13 @@ function App() {
               <Route path='/perfilTatuadores' exact element= {<PageProfileT/>}/>
               <Route path='/citas' exact element= {<PageQuotes/>}/>
               <Route path='/tatuadores' exact element= {<PageTattooists/>}/>
+
+
               <Route path='/IngresarSesion' exact element= {<PageFormSe/>}/>
-              <Route path='/usuario' exact element={<PageProfileU/>}></Route>
+
+
+              <Route path='/Piercings' exact element= {<PagePiercings/>}/>
+
           </Routes>
       <Footer/>
     </div>

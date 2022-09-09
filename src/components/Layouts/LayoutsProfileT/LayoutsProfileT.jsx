a2ed93570c0adef5d13004726694e9914e8bf928
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-09-09 13:22:14 -0500

    se modifico el perfil del tatuador en rama junda112

--- src/components/Layouts/LayoutsProfileT/LayoutsProfileT.jsx
+++ src/components/Layouts/LayoutsProfileT/LayoutsProfileT.jsx
@@ -1,49 +1,51 @@
 import React from 'react'
-import fondo from '../../../images/Fondos/fondoTattoopacity.jpg'
+import imgP from '../../../images/Tatuadores/tatuador1.png'
 import '../LayoutsProfileT/Profile.scss'
-import tatuadorp from '../../../images/Tatuadores/tatuador2.png'
-import porta1 from '../../../images/Tattos/portafolio1.jpg'
-import porta2 from '../../../images/Tattos/portafolio2.jpg'
-import porta3 from '../../../images/Tattos/portafolio3.jpg'
-import porta4 from '../../../images/Tattos/portafolio4.jpg'
-import porta5 from '../../../images/Tattos/portafolio5.jpg'
-import porta6 from '../../../images/Tattos/portafolio6.jpg'
-import porta7 from '../../../images/Tattos/portafolio7.jpg'
-import porta8 from '../../../images/Tattos/portafolio8.jpg'
-import porta9 from '../../../images/Tattos/portafolio9.jpg'
+import '../../../scss/base/_global.scss'
+import img1 from '../../../images/Tattos/portafolio1.jpg'
+import img2 from '../../../images/Tattos/portafolio2.jpg'
+import img3 from '../../../images/Tattos/portafolio3.jpg'
+
 
 export const LayoutsProfileT = () => {
   return (
-    <div>
-        <header className='conbanner' >
-            <img className='Banner' src={fondo} alt="" />
-            <div className='conimgp' >
-                <img className='imgp' src={tatuadorp }  alt="" />
+    <div className='contPather'>
+        <div className='contPe'>
+            <div className='contImg'>
+                <img className='Profile' src={imgP} alt="" />
             </div>
-        </header>
-        <div className='connombre' > 
-            <h2>Nombre del Tatuador</h2>
-        </div>
-        <div className='conportafolio' >
-            <h3>Mi Portafolio</h3>
-        </div>
-        <div className='contenedorT' >
+            <div className='contInfo'>
+                <div className='inf'><p>Nombre: Carlos</p></div>
+                <div className='inf'><p>Seguidores: 120.000</p></div>
+                <div className='inf'><p>Trabajos Subidos: 45</p></div>
+                <div className='inf'><p>direccion: cra5ta #19-30</p></div>
 
-            <div className='colum1' >
-                <div className='conimg' >
-                    <img src= {porta1} alt="" />
-                    <img src= {porta2} alt="" />
-                    <img src= {porta3} alt="" />
+                <div className='contButtons'>
+                    <button className='buttons_global_StyleTatto'>Añadir Cita</button>
+                    <button className='buttons_global_StyleTatto'>Seguir</button>
+                    <button className='buttons_global_StyleTatto'>Editar</button>
                 </div>
-                <div className='conimg' >
-                    <img src= {porta4} alt="" />
-                    <img src= {porta5} alt="" />
-                    <img src= {porta6} alt="" />
-                </div>
-                <div className='conimg' >
-                    <img src= {porta7} alt="" />
-                    <img src= {porta8} alt="" />
-                    <img src= {porta9} alt="" />
+            </div>
+        </div>
+        <div className='ContTrabajos'>
+            <div className='contPortafolio'>
+                <div className='contPortafolio2'>
+                    <div className='Portafolio'>
+                        <h2 className='PortafoliosTittle'>Mi Portafolios</h2>
+                    </div>
+
+                    <div className='Trabajos'>
+                        <div className='Tattos'>
+                            <img className='ContTatto' src={img1} alt="" />
+                            <img className='ContTatto' src={img2} alt="" />
+                            <img className='ContTatto' src={img3} alt="" />
+                        </div>
+                        <div className='Tattos'>
+                            <img className='ContTatto' src={img1} alt="" />
+                            <img className='ContTatto' src={img2} alt="" />
+                            <img className='ContTatto' src={img3} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

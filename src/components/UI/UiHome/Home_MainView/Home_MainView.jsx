1cfdd23aee341e35dafc2f31998ff52e61404e52
dairongranada <dairongranada21@gmail.com>
2022-09-11 16:24:48 -0500

    Cambios varios -DG

--- src/components/UI/UiHome/Home_MainView/Home_MainView.jsx
+++ src/components/UI/UiHome/Home_MainView/Home_MainView.jsx
@@ -8,8 +8,19 @@ export const Home_MainView = () => {
                     <h2 className='titleMP4'>EN TU PIEL</h2>
                 <p>¿ No tienes una cuenta ?</p>
                 <div>
-                    <a className='buttons_global_StyleTatto' href="/IngresarSesion">INICIAR SESION</a>
-                    <a className='buttons_global_StyleTatto' href="/"> REGISTRASE</a>
+                    <ul className="menu-horizontal">
+                        <li><a className='buttons_global_StyleTatto' href="/IngresarSesion">INICIAR SESION</a></li>
+                    </ul>
+                    <ul className="menu-horizontal">
+                        <li>
+                            <a href="#" className='buttons_global_StyleTatto'>REGISTRATE COMO</a>
+                                <ul className="menu-vertical">
+                                <li><a href="#">USUARIO</a></li>
+                                <li><a href="#">ARTISTA</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+
                 </div>
         </section>
     )

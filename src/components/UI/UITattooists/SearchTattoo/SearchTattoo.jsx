2f8f7b63b89cf37e3eb6244cf32781341f893319
luisa_gutierrez <luisa.gutierrez97@misena.edu.co>
2022-09-12 06:40:40 -0500

    se agrego nuevos cambios de filtro de tatuadores

--- src/components/UI/UITattooists/SearchTattoo/SearchTattoo.jsx
+++ src/components/UI/UITattooists/SearchTattoo/SearchTattoo.jsx
@@ -1,19 +1,15 @@
 import React,{useState} from 'react'
 import './SearchTattoo.scss'
-
-
-
 export const SearchTattoo = () => {
 
     const tattoo = [
-        {name: 'lewis holguin', image:'https://res.cloudinary.com/dsoovcjav/image/upload/v1660826276/ImagesTattoo/tatuador1_rvghbe.png',descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum eius laboriosam aut quos, vitae deserunt a nisi qui esse?' },
+        {name: 'lewis holguin', image:'https://res.cloudinary.com/dsoovcjav/image/upload/v1660826276/ImagesTattoo/tatuador1_rvghbe.png',descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum eius laboriosam aut quos, vitae deserunt a nisi qui esse?'},
         {name: 'chris nuñez', image:'https://res.cloudinary.com/dsoovcjav/image/upload/v1660826273/ImagesTattoo/tatuador2_efzh1a.png', descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum eius laboriosam aut quos, vitae deserunt a nisi qui esse?'},
         {name: 'oliver peck', image:'https://res.cloudinary.com/dsoovcjav/image/upload/v1660826274/ImagesTattoo/tatuador3_jd7kt9.png',descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum eius laboriosam aut quos, vitae deserunt a nisi qui esse?' },
         {name: 'lisa orth', image:'https://res.cloudinary.com/dsoovcjav/image/upload/v1660826299/ImagesTattoo/tatuador4_i7gdfl.jpg', descripcion:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum eius laboriosam aut quos, vitae deserunt a nisi qui esse?' },
     ]
 
     const [searchTattoo, setsearchTattoo] = useState(tattoo)
-
     const filterTattoo = (event) =>{
         const data = event.target.value;
         const filter = tattoo.filter(tattoo => tattoo.name.includes(data))
@@ -24,32 +20,24 @@ export const SearchTattoo = () => {
     <div className='Content_Tattooists'>
       <div className='Content_TattooistsTitle'>
         <h1>ARTISTAS</h1>
-
         <p>Contamos con un equipo de artistas profesionales e integrales que se 
           desempeñan en las diferentes técnicas o estilos del tatuaje, entre ellos, 
           sombras, realismo, sombras, color, realismo color, oriental, tradicional, 
           neotradicional, libre, liners, black work, black and grey y cover up</p>
       </div>
       <input className="Filter_Tatto" onChange={filterTattoo} type="text" placeholder='Buscar Artista' />
-      
-      
-      
-    
-
+  
       <main className='main-contenido'>
-        
         {searchTattoo.map((tattoo, index) => {
             return<div className="our-team-section" key={index}>
                   <div className="member-container">
-                      <img src={tattoo.image} alt={tattoo.name} />
+                      <img className='imgtattooists' src={tattoo.image} alt={tattoo.name} />
                       <h3 className="member-name">{tattoo.name}</h3>
                       <p className="member-description">{tattoo.descripcion}</p>
                       <a href="/perfilTatuadores" className="button">VER MI PERFIL!</a>
                   </div>
                 </div>
               })}
-
-
       </main>
     </div>
   )

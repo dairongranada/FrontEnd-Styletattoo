f8e459c2dd9748a9d6232ea045e3526d93724f8a
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-09-14 15:58:07 -0500

    se empezo el desarrollo del perfil de usuario -JD -SLM

--- src/components/Layouts/LayoutsProfileU/LayoutsProfileU.jsx
+++ src/components/Layouts/LayoutsProfileU/LayoutsProfileU.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import user1 from '../../../images/Tatuadores/user1.jpg'
 import './LayoutsProfileU.scss'
+
 
 
 export const LayoutsProfileU = () => {
   return (
     <div className='Content_profleUser'>
-
-
-      
+        <div className='FaceUser'>
+          <div className='contImgUser'>
+            <img className='User' src={user1} alt="" />
+            <div>
+              <a href=""><span class="material-symbols-outlined">edit</span></a>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }

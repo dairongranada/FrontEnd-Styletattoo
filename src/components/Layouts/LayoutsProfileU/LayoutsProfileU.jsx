1a8de7cb5804d59ed8b7c1ccdb1b84b6030ea210
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-09-26 09:12:53 -0500

    se modifico el perfil de usuario -JD

--- src/components/Layouts/LayoutsProfileU/LayoutsProfileU.jsx
+++ src/components/Layouts/LayoutsProfileU/LayoutsProfileU.jsx
@@ -24,69 +24,74 @@ const confirmUp = () => {
 }
   return (
       <div className='Content_profleUser'>
-        <div className='FaceUser'>
-          <div className='contImgUser'>
-            <img className='User' src={user1} alt="" />
-            <div className='contLa'>
-              <a onClick={() => EditProfileImg()}  href="#"><span  className="material-symbols-outlined">edit</span> Editar</a>
+
+        <div className='contProfileU'>
+          <div className='ContFaceUser'>
+            <img className='FaceUser' src={user1} alt="" />
+          </div>
+          <div className='contNameU'>
+            <h3 className='NameU'>laura sofia</h3>
+          </div>
+          <div className='optionsUser'>
+            <div className='option'>
+              <p>edit Photo</p>
+            </div>
+            <div className='option'>
+              <p>modif name</p>
+            </div>
+            <div className='option'>
+              <p>edit password</p>
+            </div>
+            <div className='option'>
+              <p>modif email</p>
+            </div>
+            <div className='option'>
+              <p>close session</p>
             </div>
           </div>
-          <div className='contInfoUser'>
-            <h3>Laura Sofia</h3>
-            <p>laurasofia1450@gmail.com</p>
-            {/* <p>3145784958</p> */}
+        </div>
+
+        <div className='contPublicP'>
+          <div className='contTittle'>
+            <h3>Public Profile</h3>
+            <p>add information about yourself</p>
+            <hr />
           </div>
-        </div>
-      
 
-        <div className='agendas'>
-          <div className='contAgendas'>
-            <div className='InfoUser'>
-              <h3>CITAS AGENDADAS</h3>
+          <div className='datesBasics'>
+            <div className='contN'>
+            <label htmlFor="text">Name</label>
+              <input className='boxInf' type="text"  value='laura'/>
             </div>
-
-            <div className='contCitasC'>
-              <div className='cit'>
-                <img className='TatuadorC' src={tatuador2} alt="" />
-                <div className='Calend'>
-                  <div className='infor'>
-                    <p>Fecha : 31/12/2022</p>
-                    <p>Hora : 6:00 pm</p>
-                    <p>Direccion : cra5ta #20-52</p>
-                    <button className='buttons_global_StyleTatto'>CANCELAR CITA</button>
-                  </div>
-                </div>
-              </div>
-
-              <div className='cit'>
-                <img className='TatuadorC' src={tatuador1} alt="" />
-                <div className='Calend'>
-                  <div className='infor'>
-                    <p>Fecha : 22/09/2022</p>
-                    <p>Hora : 3:00 pm</p>
-                    <p>Direccion : cra5ta #19-30</p>
-                    <button className='buttons_global_StyleTatto'>CANCELAR CITA</button>
-                  </div>
-                </div>
-              </div>
+            <div className='contN'>
+              <label htmlFor="password">password</label>
+              <input className='boxInf' type="text" value='1234'/>
             </div>
-          </div>    
-        </div>
-        <div id='editprofile' className='modalImg'>
-            <div className='contButtonUp'>
-                <h2>Editar perfil</h2>
-                <div id='contaianputUp' className='buttons_global_StyleTatto'>
-                    <p >Actualizar foto de perfil</p>
-                    <input className='confirmarUp' type="file" />
-                </div>
-                <svg id='close' onClick={() => ocultarImageUp()} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
-                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
-                </svg>
-                <button onClick={() => confirmUp()} id='btnConfirmar' className='buttons_global_StyleTatto'>Confirmar</button>
-                <button  id='btneditemail' className='buttons_global_StyleTatto'>Editar correo</button>
-                <button  id='btneditname' className='buttons_global_StyleTatto'>Editar Nombre</button>
+            <div className='contN'>
+              <label htmlFor="email">Email</label>
+              <input className='boxInf' type="text" value='Hedline'/>
             </div>
+          </div>
+          <div className='contButtonSave'>
+            <button className='buttons_global_StyleTatto'>Save</button>
+          </div>
         </div>
       </div>
   )
 }
+
+// {/* <div id='editprofile' className='modalImg'>
+//             <div className='contButtonUp'>
+//                 <h2>Editar perfil</h2>
+//                 <div id='contaianputUp' className='buttons_global_StyleTatto'>
+//                     <p >Actualizar foto de perfil</p>
+//                     <input className='confirmarUp' type="file" />
+//                 </div>
+//                 <svg id='close' onClick={() => ocultarImageUp()} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+//                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+//                 </svg>
+//                 <button onClick={() => confirmUp()} id='btnConfirmar' className='buttons_global_StyleTatto'>Confirmar</button>
+//                 <button  id='btneditemail' className='buttons_global_StyleTatto'>Editar correo</button>
+//                 <button  id='btneditname' className='buttons_global_StyleTatto'>Editar Nombre</button>
+//             </div>
+//         </div> */}

28ce8d62c8dbe0d0154ff8984761b881cd9ccda3
samuel <samuelelrey0126yt@gmail.com>
2022-09-28 07:33:46 -0500

    agrege nuevos cambios al page consejos

--- src/components/Pages/__PageConsejos/PageConsejos.jsx
+++ src/components/Pages/__PageConsejos/PageConsejos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import tatu from '../../../images/Tatuadores/tatuador11.jpg'
+import piercing from '../../../images/Tattos/piercing12.jpg'
 import './PageConsejos.scss'
 
 
@@ -9,7 +10,9 @@ export const PageConsejos = () => {
       <div className='contseondConsejos'>
         <h3>Consejos</h3>
         <img className='imgconsejos' src={tatu} alt="" />
-        <li><a className='buttons_global_StyleTatto' href="/artistas">LEER MAS SOBRE</a></li>
+        <li><a className='buttons_global_StyleTatto' href="/artistas">LEER MAS SOBRE TATUAJES</a></li>
+        <img className='imgconsejos2' src={piercing} alt="" />
+        <li><a className='buttons_global_StyleTatto' href="/artistas">LEER MAS SOBRE PIERCINGS</a></li>
       </div>
     </div>
   )

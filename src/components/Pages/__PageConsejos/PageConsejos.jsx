09d7e48fcf85ebf0b073b368f2e3cf81f5a376d9
samuel <samuelelrey0126yt@gmail.com>
2022-09-28 06:57:16 -0500

    agrege cambios al page consejos

--- src/components/Pages/__PageConsejos/PageConsejos.jsx
+++ src/components/Pages/__PageConsejos/PageConsejos.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import tatu from '../../../images/Tatuadores/tatuador11.jpg'
+import './PageConsejos.scss'
 
 
 export const PageConsejos = () => {
   return (
-    <div>Consejos</div>
+    <div className='contConsejos' >
+      <div className='contseondConsejos'>
+        <h3>Consejos</h3>
+        <img className='imgconsejos' src={tatu} alt="" />
+      </div>
+    </div>
   )
 }

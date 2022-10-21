14e20796d9dc04f10f9c81fb211c42ca8ef28331
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-10-21 08:26:07 -0500

    se a adelantado el trabajo del rediseño del nav -JD

--- src/components/Layouts/NavigationBar/NavigationBar.jsx
+++ src/components/Layouts/NavigationBar/NavigationBar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavigationBar.scss'
+import perfilUsuarioAnonim from '../../../images/Icons/perfilUsuarioAnonim.jpg'
 import { ImgTemplate } from '../../UI/ImgTemplate/ImgTemplate';
 import logo from '../../../images/Icons/logo2.png'
 import Cookies from 'universal-cookie';
+import { useEffect } from 'react';
 
 const cookies = new Cookies();
 
@@ -17,20 +19,61 @@ export const NavigationBar = ({classN, href, text}) => {
         } 
     }
 
+    // E F E C T O    D E L    S C R O L L
+
+    const [show, handleShow] = useState()
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+
+            if(window.scrollY > 80){
+                handleShow(true)
+                // console.log(show)
+            }else handleShow(false)
+        })
+        // return () => {
+        //     window.removeEventListener("scroll")
+        // }
+        window.addEventListener('load', () => {
+            const modalDesp = document.getElementById('despleg')
+            modalDesp.style.visibility = "hidden"
+        })
+    },[])
+    
+
+    const despliege = () => {
+        const modalDesp = document.getElementById('despleg')
+        modalDesp.style.visibility = "visible"
+    }
+
+
+
     return (
-        <div className='barNavigate'>            
-            <nav className='menu'id='js-nav'>
-                <a href="/"><ImgTemplate srcImg={logo} className='logo' alt=''  /></a>   
-                <ul className='menu-items'>
-                    <li><a className='option2' href="/" >INICIO</a></li>
-                    <li><a className='option2' href="/artistas">ARTISTAS</a></li>
-                    {
-                    1<3? (
-                            <li><a className= "option2" href="/user/edit-profile">Perfil</a></li>
-                        ):("")}
-                </ul>
-                <span onClick={menu} id='burguer' className="material-symbols-outlined">menu</span>
-            </nav>
-        </div>
+        <>
+            <div className='barNavigate'>            
+                <nav className={`menu ${show && "nav_black"}`}id='js-nav'>
+                    <div className='contOptionsNav'>
+                        <a href="/"><ImgTemplate srcImg={logo} className='logo' alt=''  /></a>
+                        
+                        <ul className='menu-items'>
+                            <li><a className='option2' href="/" >INICIO</a></li>
+                            <li><a className='option2' href="/artistas">ARTISTAS</a></li>
+                            {
+                            1<3? (
+                                    <li><a className= "option2" href="/user/edit-profile">Perfil</a></li>
+                                ):("")}
+                        </ul>
+                    </div>
+                    <img id='logoP' className='logo' src={perfilUsuarioAnonim} onClick={despliege} alt="" />
+                    <span onClick={menu} id='burguer' className="material-symbols-outlined">menu</span>
+                </nav>
+                <div id='despleg' className='despleg'>
+                    <ul>
+                        <li>Perfil</li>
+                        <li>Cerrar Sesion</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }

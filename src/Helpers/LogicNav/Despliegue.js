e5d3674ca7d68c87deea66e5cdd4e51f383f9058
dairongranada <dairongranada21@gmail.com>
2022-10-21 11:22:18 -0500

    Corrections -DG

--- src/Helpers/LogicNav/Despliegue.js
+++ src/Helpers/LogicNav/Despliegue.js
@@ -1,12 +1,3 @@
 
 
 // MENU BURGER
-export const menuBurguer = (btnMenu,btnItems ) => {
-    const btn_menu = document.querySelector(btnMenu)
-    if (btn_menu) {
-        const menu_items = document.querySelector(btnItems)
-        menu_items.classList.toggle('open')
-    }
-
-    console.log(btnMenu,btnItems);
-}
--- src/components/Layouts/NavigationBar/NavigationBar.jsx
+++ src/components/Layouts/NavigationBar/NavigationBar.jsx
@@ -5,22 +5,24 @@ import { ImgTemplate } from '../../UI/ImgTemplate/ImgTemplate';
 import logo from '../../../images/Icons/logo2.png'
 import Cookies from 'universal-cookie';
 import { useEffect } from 'react';
+import { AiOutlineUser } from 'react-icons/ai';
+import { BiUpArrow } from "react-icons/bi";
 
-import { menuBurguer } from '../../.././Helpers/LogicNav/Despliegue'
 
 const cookies = new Cookies();
 
 
-export const NavigationBar = () => {
+export const NavigationBar = ({classN, href, text}) => {
 
-    menuBurguer('material-symbols-outlined','menu-items')
-
-    
-
-    const despliege = () => {
-        const modalDesp = document.getElementById('despleg')
-        modalDesp.style.visibility = "visible"
+    const menu = ()=>{
+        const btn_menu = document.querySelector('.material-symbols-outlined')
+        if(btn_menu){
+            const menu_items = document.querySelector('.menu-items')
+            menu_items.classList.toggle('open')
+        } 
     }
+
+
 
 
 
@@ -29,19 +31,44 @@ export const NavigationBar = () => {
             <div className='barNavigate'>            
                 <nav className='menu' id='js-nav'>
                     <div className='contOptionsNav'>
-                        <a href="/"><ImgTemplate srcImg={ logo } className='logoStyleTatto' alt='' /></a>
+                        <a href="/"><ImgTemplate srcImg={logo} className='logoStyleTatto' alt=''  /></a>
                         
                         <ul className='menu-items'>
-                            <li><a className='optionNavBar' href="/" >INICIO</a></li>
-                            <li><a className='optionNavBar' href="/artistas">ARTISTAS</a></li>
-                            {
-                            1<3? (
-                                    <li><a className= "optionNavBar" href="/user/edit-profile">Perfil</a></li>
-                                ):("")}
+                            <li><a className='option2' href="/" >INICIO</a></li>
+                            <li><a className='option2' href="/artistas">ARTISTAS</a></li>
                         </ul>
                     </div>
-                    <img id='logoPrfile' className='ImgUserProfile' src={perfilUsuarioAnonim} onClick={despliege} alt="" />
-                    <span onClick={menuBurguer()} id='burguer' className="material-symbols-outlined">menu</span>
+                    <span onClick={menu} id='burguer' className="material-symbols-outlined">menu</span>
+
+
+
+                {/*  ICON PERFIL  USUARIO....  */ }
+                    <ul class="menu-horizontal">
+                        <li>
+                            <a href="#"><img id='logoP' className='ImgUserProfile' src={perfilUsuarioAnonim} alt="" /></a>
+                            <ul class="menu-vertical">
+                                <div className='UpArrow'><BiUpArrow/></div>
+                                <li className='menuOpcionsNav'><a href="/user/edit-profile"><AiOutlineUser/> Cuenta</a></li>
+                                <li className='menuOpcionsNav'><a href="#">Citas Agendadas</a></li>
+                                <ul className='cerrarSesionUL'><li className='menuOpcionsNav'><a href="#">Cerrar sesion</a></li></ul>
+                            </ul>
+                        </li>
+                    </ul>
+                {/*  ICON PERFIL  TATUADR....  */ }
+                {/* <ul class="menu-horizontal">
+                        <li>
+                            <a href="#"><img id='logoP' className='ImgUserProfile' src={perfilUsuarioAnonim} alt="" /></a>
+                            <ul class="menu-vertical">
+                                <li><a href="#"><AiOutlineUser/> Cuenta</a></li>
+                                <li><a href="#">Citas Agendadas</a></li>
+                                <li><a href="#">Cerrar sesion</a></li>
+                            </ul>
+                        </li>
+                    </ul> */}
+
+
+
+                
                 </nav>
                 <div id='despleg' className='despleg'>
                     <ul>

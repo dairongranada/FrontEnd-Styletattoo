fb30f27e13d164ee9433bc1d60cf261787648ef9
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-10-28 11:00:09 -0500

    rutas

--- src/components/UI/UiHome/Home_NewTattooists/Home_NewTattooists.jsx
+++ src/components/UI/UiHome/Home_NewTattooists/Home_NewTattooists.jsx
@@ -21,7 +21,7 @@ export const Home_NewTattooists = () => {
                 <Home_CardsTattooists  ImgTattooists={ImgTattooistsTwo}  NameTattooists='Lorem' SocialTattooists='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum alias est delectus culpa?'/>
                 <Home_CardsTattooists  ImgTattooists={ImgTattooistsThree}  NameTattooists='lorem2' SocialTattooists='Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta officia tempora eveniet alias?'/>
             </div>
-            <div className='SeeTattooists'><a className='buttons_global_StyleTatto' href="/artistas">VER MAS</a></div>
+            <div className='SeeTattooists'><a className='buttons_global_StyleTatto' href="/all/artist/style">VER MAS</a></div>
         </section>
 
     )

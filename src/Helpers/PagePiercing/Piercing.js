9ae0586696ec1d33886d647419bcd6cafd651402
samuel <samuelelrey0126yt@gmail.com>
2022-11-08 10:13:11 -0500

    actualice page piercing

--- src/Helpers/PagePiercing/Piercing.js
+++ src/Helpers/PagePiercing/Piercing.js
@@ -4,6 +4,9 @@ import img3 from '../.././images/Piercing/piercing3.jpg'
 import img4 from '../.././images/Piercing/piercing4.jpg'
 import img5 from '../.././images/Piercing/piercing5.jpg'
 import img6 from '../.././images/Piercing/piercing6.jpg'
+import img7 from '../.././images/Piercing/piercing7.jpg'
+import img8 from '../.././images/Piercing/piercing8.jpg'
+import img9 from '../.././images/Piercing/piercing9.jpg'
 
 
 
@@ -14,4 +17,7 @@ export const Piercing = [
     {Image: img4},
     {Image: img5},
     {Image: img6},
+    {Image: img7},
+    {Image: img8},
+    {Image: img9},
 ]

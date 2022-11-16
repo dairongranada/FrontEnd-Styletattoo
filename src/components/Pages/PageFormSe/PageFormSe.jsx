99bf3aecebff128c7f3bb0e7d5228f4bea4b3e29
dairongranada <dairongranada21@gmail.com>
2022-11-16 16:02:01 -0500

    Correccion LOGIN -DG

--- src/components/Pages/PageFormSe/PageFormSe.jsx
+++ src/components/Pages/PageFormSe/PageFormSe.jsx
@@ -66,7 +66,7 @@ export const PageFormSe = () => {
                                 localStorage.setItem("InfoUser", JSON.stringify( InfoUser));
 
                                 localStorage.setItem("usuario", JSON.stringify(parseJwt( tokenInfo, rol )) );
-                                const data = info.data.info.info_user;
+                                const data = info.data.info;
         
 
                                 console.log(data);

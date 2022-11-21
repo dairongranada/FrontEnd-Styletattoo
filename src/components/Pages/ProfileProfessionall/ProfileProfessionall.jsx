109df90f8c02b84d47029b1f61fea6c4232c47d1
dairongranada <dairongranada21@gmail.com>
2022-11-21 14:29:26 -0500

    Correcion Del Error 3.8 -DG

--- src/components/Pages/ProfileProfessionall/ProfileProfessionall.jsx
+++ src/components/Pages/ProfileProfessionall/ProfileProfessionall.jsx
@@ -1,23 +1,35 @@
-import React, { useState,useEffect } from "react";
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import './ProfileProfessionall.scss'
+import './citas.scss'
+
+import { getAllTatuadoresID } from '../../../Helpers/ApiConsumer/Tattuadores'
+
+
 import { useNavigate, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
 
 import { motion } from "framer-motion"
-import './ProfileProfessionall.scss'
 
+import { Portafolio } from './Portafolio';
 import { NavFooter } from '../../Layouts/NavigationFooter/NavFooter/NavFooter';
 import { NavigationBar } from '../../Layouts/NavigationBar/NavigationBar';
-import {Portafolio  } from './Portafolio';
-
-import {  getAllTatuadoresID } from '../../../Helpers/ApiConsumer/Tattuadores'
+import { signUpUser } from '../../../Helpers/ApiConsumer/AuthRegistro';
 
 
-import imgStyle from '../../../images/Tatuadores/tatuador1.png'
 import { GoHeart } from 'react-icons/go';
-const baseURL = "https://rickandmortyapi.com/api/character";
+
+
 
 
 export const ProfileProfessionall = () => {
+
+
+
+  const [serverError, setServerError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [duplicatedData, setDuplicatedData] = useState(false);
+  const [registered, setRegistered] = useState(false);
 
   const { id } = useParams();
   const idTT = id
@@ -33,8 +45,8 @@ export const ProfileProfessionall = () => {
   const [tatuadores, setTatuadores] = useState([]);
   const [perfilProfesional, setperfilProfesional] = useState({});
 
-  const {first_name, last_name,email} = tatuadores
-  const {departament,description,direction ,experience,img,municipio}= perfilProfesional
+  const { first_name, last_name, email } = tatuadores
+  const { departament, description, direction, experience, img, municipio } = perfilProfesional
 
 
   const [perilPortafolio, setPerilPortafolio] = useState([]);
@@ -43,26 +55,26 @@ export const ProfileProfessionall = () => {
 
 
 
-  useEffect( () => {
-    
+  useEffect(() => {
+
     getAllTatuadoresID(idTT)
-    .then( info => {
-        setTatuadores( info.data );
+      .then(info => {
+        setTatuadores(info.data);
         setperfilProfesional(info.data.PerfilProfesional[0])
         setPerilPortafolio(info.data.Portafolio)
         setdisponiblidad(info.data.iDispo[0].dispo)
-        
+
         if (disponiblidad == true) {
           setEstatus('switch-toggle Disponible-btn')
-        }if (disponiblidad == false) {
+        } if (disponiblidad == false) {
           setEstatus('switch-toggle Ocupado-btn')
         }
-        
-    })
+
+      })
   }, [])
 
 
-console.log(disponiblidad);
+  console.log(disponiblidad);
 
   return (
     <>
@@ -70,8 +82,8 @@ console.log(disponiblidad);
       <div className='ContentBodyInfoTattois'>
         <div className='RightInftoTT'>
           <div className='ImgRightInftoTT'><img src={img} alt="" /></div>
-          <p style={{display:"flex",fontSize:"15px",marginTop:"0.6rem",alignItems: "center"}} ><span  style={{fontSize: "19px"}} className="material-symbols-outlined">location_on</span>{municipio} -- {departament}</p>
-          <p style={{display:"flex",fontSize:"15px",alignItems: "center"}}>Direccion:  { direction}</p>
+          <p style={{ display: "flex", fontSize: "15px", marginTop: "0.6rem", alignItems: "center" }} ><span style={{ fontSize: "19px" }} className="material-symbols-outlined">location_on</span>{municipio} -- {departament}</p>
+          <p style={{ display: "flex", fontSize: "15px", alignItems: "center" }}>Direccion:  {direction}</p>
           <div className='ButtonsRightInftoTT'>
             <button className='Btn-Citas buttons_global_StyleTatto'>AGENDAR CITA</button>
             <button className='tooltip buttonLike'>
@@ -84,8 +96,8 @@ console.log(disponiblidad);
               <div className="switch-label">
                 <i className="fa fa-bluetooth-b"></i><span>Servicio</span>
               </div>
-              <div className={`${ disponiblidad == true && "switch-toggle Disponible-btn" }  ${ disponiblidad == false && "switch-toggle Ocupado-btn"}` }>
-                <input style={{visibility: "hidden"}}  type="checkbox" id="bluetooth" />
+              <div className={`${disponiblidad == true && "switch-toggle Disponible-btn"}  ${disponiblidad == false && "switch-toggle Ocupado-btn"}`}>
+                <input style={{ visibility: "hidden" }} type="checkbox" id="bluetooth" />
                 <label htmlFor="bluetooth"></label>
               </div>
             </div>
@@ -97,60 +109,201 @@ console.log(disponiblidad);
             <div className='NameLeftInftoTT'><h5> {first_name} {last_name} </h5></div>
             <div className='EmailLeftInftoTT'><p> {email} </p></div>
             <div className='DescriLeftInftoTT'><p>{description}</p></div>
-            <div className='DescriLeftInftoTT'style={{display: "flex", gap:"5px" }}> <p style={{color:"var(--colorOrange2)"}} >Experiencia :</p><p>{experience} </p> <p>Años</p> </div>
-            </div>
+            <div className='DescriLeftInftoTT' style={{ display: "flex", gap: "5px" }}> <p style={{ color: "var(--colorOrange2)" }} >Experiencia :</p><p>{experience} </p> <p>Años</p> </div>
+          </div>
         </div>
 
         <div className='PortaFInftoTT'>
           <div className='PortaFTitle'><h5>MI TRABAJO</h5></div>
           <div className='PortaFTitleImages'>
 
-          {(perilPortafolio.length === 0) &&
-                    <>
-                      <div className="NoPortafolio">
-                      <div className="loadPortafolio  loaderArtist">
-                          <div style={{marginRight: "8rem"}} className="spinner">
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                            <span style={{width: "67px"}}  ></span>
-                          </div>
-                          <p style={{marginTop:"10px"}} >Este artista aun no tiene trabajos...</p>
-                          </div>
-                      </div>
-                    </>
-                }
+            {(perilPortafolio.length === 0) &&
+              <>
+                <div className="NoPortafolio">
+                  <div className="loadPortafolio  loaderArtist">
+                    <div style={{ marginRight: "8rem" }} className="spinner">
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                      <span style={{ width: "67px" }}  ></span>
+                    </div>
+                    <p style={{ marginTop: "10px" }} >Este artista aun no tiene trabajos...</p>
+                  </div>
+                </div>
+              </>
+            }
 
 
 
             <motion.div className='slider-container'>
               <motion.div className='slider' drag='x'
                 dragConstraints={{ right: 0, left: -4000 }} >
-               {perilPortafolio.map(data =>(
-                  (perilPortafolio.length >=1) &&
-                    <Portafolio
-                      key={data.id}
-                      image1={data.img1}
-                      image2={data.img2}
-                      image3={data.img3}
-                      image4={data.img4}
-                      image5={data.img5}
-                    />
-                ))} 
+                {perilPortafolio.map(data => (
+                  (perilPortafolio.length >= 1) &&
+                  <Portafolio
+                    key={data.id}
+                    image1={data.img1}
+                    image2={data.img2}
+                    image3={data.img3}
+                    image4={data.img4}
+                    image5={data.img5}
+                  />
+                ))}
               </motion.div>
             </motion.div>
           </div>
         </div>
 
-        <div className="ContentHorarioBack">    
-          <div><h2>HORARIO</h2></div>      
-          <div className="loader"><span className="hour"></span><span className="min"></span><span className="circel"></span></div>
+        <div className="ContentTimeBackgournd">
+          <div className="ContentHorarioBack">
+            {/* <div className="loader"><span className="hour"></span><span className="min"></span><span className="circel"></span></div> */}
+
+
+
+
+
+
+            <div style={{ marginLeft: "-4rem" }} className='Content_FormsPrincipal'>
+              <Formik
+                initialValues={{
+                  username: "",
+                  first_name: "",
+                  last_name: "",
+                  cellPhone: "",
+                  email: "",
+                  password: "",
+                  passwordConfirm: '',
+                  rol: "[ROLE_USUARIO]",
+                }}
+
+                validate={(valores) => {
+                  let ers = {}
+                  // VALIDACION NOMBRE
+                  if (!valores.first_name) {
+                    ers.first_name = "Porfavor ingresa un nombre"
+                  } else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.first_name)) {
+                    ers.first_name = "Porfavor ingresa solo letras"
+                  }
+
+                  return ers
+
+                }}
+
+                onSubmit={(valores, { resetForm }) => {
+                  let validacion = {};
+                  let username = valores.first_name + valores.last_name
+
+                  signUpUser({
+                    username: username,
+                    first_name: valores.first_name,
+                    last_name: valores.last_name,
+                    cellPhone: valores.cellPhone,
+                    email: valores.email,
+                    password: valores.password,
+                    rol: "[ROLE_USUARIO]",
+                    image: "https://i.postimg.cc/T2N5CnwK/perfil-Usuario-Anonim.png"
+
+                  }).then(info => {
+                    validacion = info
+                    setLoading(true);
+                    if (validacion.status === 400) {
+                      setDuplicatedData(true);
+                      setServerError(false);
+                      setLoading(false);
+                    }
+                    else if (validacion.status === 500) {
+                      setServerError(true);
+                      setDuplicatedData(false);
+                      setLoading(false);
+                    }
+                    else {
+                      setDuplicatedData(false);
+                      resetForm();
+                      setLoading(false);
+                      setRegistered(true);
+                      window.location = "/IngresarSesion";
+                    }
+                  });
+
+
+                }}
+
+
+              >
+                {({ errors, touched }) => (
+                  <Form className='formCitas'>
+                    <h2>Agenda tu cITA</h2>
+                    <div className="inputContentCitas">
+                      <div>
+                        <Field
+                          className='inputsCitas'
+                          name='first_name'
+                          id='first_name'
+                          type="date"
+                          placeholder='date'
+                        />
+                        {touched.name && errors.name && <span>{errors.name}</span>}
+                      </div>
+                      <div>
+                        <Field
+                          className='inputsCitas'
+                          name='Time'
+                          id='Time'
+                          type="Time"
+                        />
+                        {touched.last_name && errors.last_name && <span>{errors.last_name}</span>}
+                      </div>
+                    </div>
+
+                    <div className="inputContentCitas">
+                      <div>
+                        <Field
+                          className='inputsCitas'
+                          name='file'
+                          id='file'
+                          type="file"
+                          placeholder='Correo'
+                        />
+                        {touched.email && errors.email && <span>{errors.email}</span>}
+
+                      </div>
+                    </div>
+                    <div className="inputContentCitas">
+
+                      <div>
+                        <Field
+                          className='inputsCitas'
+                          name='Description'
+                          id='Description'
+                          as="textarea"
+                          placeholder='Muestrame tu idea'
+                          maxLength='150'
+                        />
+                        {touched.cellPhone && errors.cellPhone && <span>{errors.cellPhone}</span>}
+                      </div>
+                    </div>
+
+
+                    <div className='Btn_Citas'>
+                      <button
+                        className="buttons_global_StyleTatto"
+                        type={"submit"}>
+                        Agendar Cita
+                      </button>
+                    </div>
+
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          </div>
         </div>
       </div>
+
     </>
   )
 }

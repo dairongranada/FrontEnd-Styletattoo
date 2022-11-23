3d4748f591691353558dffcf883f596a143c0a6c
dairongranada <dairongranada21@gmail.com>
2022-11-23 16:15:55 -0500

    Correcion Del Error 4.7 -DG

--- src/components/UI/Up/Up.jsx
+++ src/components/UI/Up/Up.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import'./Up.scss'
+import './Up.scss'
 
 export const Up = () => {
     const FunctionScroll = () => {
         window.scroll({
-          top: 0,
-          behavior: 'smooth'
+            top: 0,
+            behavior: 'smooth'
         })
-      }
+    }
     return (
 
-        <p onClick={FunctionScroll}  id="edgtf-back-to-top" className="on">
+        <p onClick={FunctionScroll} id="edgtf-back-to-top" className="on">
             <span className="edgtf-icon-stack">
-                <i style={{color: "#fff"}} className="edgtf-icon-ion-icon ion-android-arrow-dropup material-symbols-outlined">expand_less</i>                
+                <i style={{ color: "#fff" ,fontSize: "30px",transform: "translate(0px, 2px)" }} className="edgtf-icon-ion-icon ion-android-arrow-dropup material-symbols-outlined">arrow_drop_up</i>
             </span>
-		    <span className="edgtf-btt-text">Subir</span>
-		</p>
-        
+            <span className="edgtf-btt-text">Subir</span>
+        </p>
+
 
     )
 }

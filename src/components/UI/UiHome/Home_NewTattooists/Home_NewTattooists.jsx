4bdaaa6cdb2ec820e8da4dca199d95757b430a5e
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-11-29 17:10:31 -0500

    validacion terminada de artistas del momento terminada -JD

--- src/components/UI/UiHome/Home_NewTattooists/Home_NewTattooists.jsx
+++ src/components/UI/UiHome/Home_NewTattooists/Home_NewTattooists.jsx
@@ -30,12 +30,12 @@ export const Home_NewTattooists = () => {
 
     return (
         <section className='NewTattooists'>
-            {(allTaoits.length >= 1)&&
+            {(allTaoits.length >= 1 && allTaoits.like >= 1)&&
                 <div>
                     <h1 style={{marginTop:"2rem"}} className='titleMP4'>Artistas del momento</h1>
                 </div>
             }
-            
+
             <div className="containerCardsArts">
                 {allTaoits.map(dat => (
                     (dat.PerfilProfesional.length >=1 && dat.iDispo[0].like >= 1) &&

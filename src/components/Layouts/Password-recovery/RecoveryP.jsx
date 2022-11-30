69252e8094dbcfe1b3662d54749cbdfed3377b4d
dairongranada <dairongranada21@gmail.com>
2022-11-30 16:01:51 -0500

    Correcion Del Error 5.3 -DG

--- src/components/Layouts/Password-recovery/RecoveryP.jsx
+++ src/components/Layouts/Password-recovery/RecoveryP.jsx
@@ -5,18 +5,31 @@ import emailjs from '@emailjs/browser';
 
 export const RecoveryP = () => {
 
+    // GENERAR CODIGO DE VERIFICACION DE CORREO 
+
+    function getRandomInt(min,max){
+        return Math.floor(Math.random() * (max -min) + min);
+      }
+
+      let CodigoParaEmail = (getRandomInt(11111,9999))
+      console.log(+CodigoParaEmail)
+
+
    
     const sendEmail = (event)=>{
         event.preventDefault();
         /// ALERTA CHIMBA
       emailjs.sendForm('service_2ubfxp4','template_kw2sbzv',event.target,'xn_UfOyxzbh71P4TH')
-        .then(response => console.log(response))
+        .then(response => (
+            console.log(response.status))
+            
+        )
+
     
-       setTimeout(() => {
-            window.location.reload(false);
-        }, 2000);
-        
- }
+    //    setTimeout(() => {
+    //         window.location.reload(false);
+    //     }, 2000);
+    }
 
     return (
         <div className='ContentcARDrECOVERYpASS'>
@@ -25,7 +38,7 @@ export const RecoveryP = () => {
                 <p className='TextParrafo'>Por favor introduce tu correo electrónico<br/> para recuperar tu contraseña</p>
                 <form className='FormRecovery' onSubmit={sendEmail} >
                     <div className="field">
-                        <input autocomplete="off" id="logemail" placeholder="Email" className="input-field" name="logemail" type="email"/>
+                        <input autoComplete="off" id="logemail" placeholder="Email" className="input-field" name="logemail" type="email"/>
                     </div>
                     <button className="buttons_global_StyleTatto" type="submit">Enviar</button>
                 </form>

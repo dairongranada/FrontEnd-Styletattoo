14d01d1f8a057cd9698c5c558e19c7ad78a1c585
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2022-12-06 10:42:12 -0500

    se actualizaron estilos para las cards de pagetattoits -JD

--- src/components/Pages/PageTattooists/CardTatuadores.jsx
+++ src/components/Pages/PageTattooists/CardTatuadores.jsx
@@ -2,25 +2,55 @@ import React from 'react'
 
 export const CardTatuadores = ({id,image,first_name,last_name ,descripcion,departament,municipio}) => {
   return (
-        <div className='pather'>
-              <div className="containerTattois">
-                <div className="cardsTattooists">
-                    <div className="face facel">
-                        <div className="content">
-                            <img src={image} alt="#" />
-                        </div>
+        <>
+            <div className="cardArtistptt">
+                <div className="miniBannerArtist">
+                    <div className='contentImgCadrArtistpgt'>
+                       <img className='ImgProfleCardPGT' src={image} alt="..." /> 
                     </div>
-                    <div className="face face2">
-                        <div className="content">
-                            <p className='location_on'>{municipio} -- {departament}</p>
-                            <p className='nameTattooists' >{first_name} {last_name}</p>
-                            <p className='descripTattooists'href="/">{descripcion}</p>
+                </div>
+                <div className="contentInfoartist">
+                    <div className='SecondContenInfoTpg'>
+                         <p>{municipio}{" "}{departament}</p>
+                         <p>{first_name} {last_name}</p>
+                    </div>
+                    <div className="rating">
+                        <span class="material-symbols-outlined">star</span>
+                        <span class="material-symbols-outlined">star</span>
+                        <span class="material-symbols-outlined">star</span>
+                        <span class="material-symbols-outlined">star</span>
+                        <span class="material-symbols-outlined">star</span>
+                    </div>
+                    <div className='contentButtons'>
+                        <div className="ButtonViewProfileApt">
                             <a href={`/tatto/view/profile/${id}`} className='btnA_menu'>Ver perfil</a>
+                        </div>
+                        <div className="ButtonViewProfileApt">
+                            <a href={`/tatto/view/profile/${id}`} className='btnA_menu'>Agendar cita</a>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
+        // <div className='pather'>
+        //       <div className="containerTattois">
+        //         <div className="cardsTattooists">
+        //             <div className="face facel">
+        //                 <div className="content">
+        //                     <img src={image} alt="#" />
+        //                 </div>
+        //             </div>
+        //             <div className="face face2">
+        //                 <div className="content">
+        //                     <p className='location_on'>{municipio} -- {departament}</p>
+        //                     <p className='nameTattooists' >{first_name} {last_name}</p>
+        //                     <p className='descripTattooists'href="/">{descripcion}</p>
+        //                     <a href={`/tatto/view/profile/${id}`} className='btnA_menu'>Ver perfil</a>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
 
   )
 }

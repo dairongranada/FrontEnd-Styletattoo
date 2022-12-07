40e6cc3ed36acd4ce2222af789348fdb2d7afc6a
dairongranada <dairongranada21@gmail.com>
2022-12-07 13:18:20 -0500

    Correcion Del Error 6.0 -DG

--- src/components/Layouts/NavigationBar/NavigationBar.jsx
+++ src/components/Layouts/NavigationBar/NavigationBar.jsx
@@ -1,5 +1,4 @@
 import { useState , useEffect } from 'react'
-import perfilUsuarioAnonim from '../../../images/Icons/perfilUsuarioAnonim.jpg'
 import { getAllTatuadoresID }  from '../../../Helpers/ApiConsumer/Tattuadores';
 import Maquuina from '../../../images/Icons/Maquuina.png'
 import Pirncings from '../../../images/Icons/Pirncings.png'
@@ -25,11 +24,6 @@ export const NavigationBar = () => {
   
     }, [])
 
-
-
-
-
-
     const handleLogout = () => {
         localStorage.removeItem("token");
         localStorage.removeItem("usuario");
@@ -38,27 +32,6 @@ export const NavigationBar = () => {
         window.location = "/IngresarSesion";
    }
    
-
-
-
-
-
-    const [barMenuIcon, setBarMenuIcon] = useState("bx bx-menu")
-    const [navClose, setNavClose] = useState(false)
-
-    const handleChangeNavs = () => {
-        if (navClose === false) {
-            setSidebarnav("open")
-            setBarMenuIcon("bx bx-menu-alt-right")
-            setNavClose(true)
-        }else{
-            setSidebarnav("")
-            setBarMenuIcon("bx bx-menu")
-            setNavClose(false)
-
-        }
-    };
-
 
   const [sidebarNav, setSidebarnav] = useState("close")
 
@@ -72,179 +45,247 @@ export const NavigationBar = () => {
 
 
   return (
-    <div>
-      <div className={`sidebar ${sidebarNav}`} >
-        <div className="logo-details">
-        <div className="home-content">
-          { sidebarNav == "close" &&
-             <i  onClick={HandleClick} style={{cursor:"pointer"}} className='bx bx-menu' ></i>
-          }
+    <>
+    {!!token && 
+      <div>
+        <div className={`sidebar ${sidebarNav}`} >
+          <div className="logo-details">
+          <div className="home-content">
+            { sidebarNav == "close" &&
+              <i  onClick={HandleClick} style={{cursor:"pointer"}} className='bx bx-menu' ></i>
+            }
 
-          { sidebarNav == "showMenu" &&
-             <i  onClick={HandleClickOpen} style={{cursor:"pointer"}} className='bx bx-menu' ></i>
-          }
-
-
-
-        </div>
-        </div>
-        <ul className="nav-links">
-          <li>
-            <a href="/">
-              <i className='bx bx-home' ></i>
-              <span className="link_name">Home</span>
-            </a>
-            <ul className="sub-menu blank">
-              <li><a className="link_name" href="#">Inicio</a></li>
-            </ul>
-          </li>
-
-
-
-        { (userData.rol === '[ROLE_ARTISTA]') &&
-          <li>
-            <div className="iocn-link">
-              <a href="/userTatto/edit-profile">
-              <i className='bx bx-user' ></i>
-                <span className="link_name">Ver perfil</span>
+            { sidebarNav == "showMenu" &&
+              <i  onClick={HandleClickOpen} style={{cursor:"pointer"}} className='bx bx-menu' ></i>
+            }
+          </div>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href="/">
+                <i className='bx bx-home' ></i>
+                <span className="link_name">Home</span>
               </a>
-              <i className='bx bxs-chevron-down arrow' ></i>
-            </div>
-            <ul className="sub-menu">
-              <li><a className="link_name" href="/userTatto/edit-profile">Perfil</a></li>
-              <li><a href="/userTatto/edit-name">Informacion</a></li>
-              <li><a href="/userTatto/edit-quotes">Citas Pendientes</a></li>
-              <li><a href="/userTatto/edit-password">Cambiar Contraseña</a></li>
-            </ul>
-          </li>
-        }
-
-        { (userData.rol === '[ROLE_USUARIO]') &&
-          <li>
-            <div className="iocn-link">
-              <a  href="/user/edit-profile">
-              <i className='bx bx-user' ></i>
-                <span className="link_name">Ver perfil</span>
-              </a>
-              <i className='bx bxs-chevron-down arrow' ></i>
-            </div>
-            <ul className="sub-menu">
-              <li><a className="link_name" href="/user/edit-profile">Perfil</a></li>
-              <li><a href="/user/edit-name">Informacion</a></li>
-              <li><a href="/user/edit-quotes">Citas Pendientes</a></li>
-              <li><a href="/user/edit-image">Cambiar Imagen</a></li>
-
-              <li><a href="/user/edit-password">Cambiar Contraseña</a></li>
-            </ul>
-          </li>
-        }
-
-          <li>
-            <a href="/all/artist/style">
-            <i className='bx bxs-user-account'></i>
-              <span className="link_name">Artistas</span>
-            </a>
-            <ul className="sub-menu blank">
-              <li><a className="link_name" href="/all/artist/style">Artistas</a></li>
-            </ul>
-          </li>
-
-
-          <li>
-            <a style={{height:"50px"}} href="/infoTatuajes">
-              <img style={{width:"25px",marginLeft:"28px" }} src={Maquuina} alt={Maquuina} />
-              <span style={{ marginLeft: "1.5rem"}} className="link_name">Tatuajes</span>
-
-            </a>
-            <ul className="sub-menu blank">
-              <li><a className="link_name" href="#">Tatuajes</a></li>
-            </ul>
-          </li>
-
-      
-          <li>
-          <a style={{height:"50px"}} href="/infoPiercings">
-              <img style={{width:"25px",marginLeft:"28px" }} src={Pirncings} alt={Maquuina} />
-              <span style={{ marginLeft: "1.5rem"}} className="link_name">Piercings</span>
-
-            </a>
-            <ul className="sub-menu blank">
-              <li><a className="link_name" href="#">Piercings</a></li>
-            </ul>
-          </li>
-
-
-          {userToken  && 
-          <li>
-            <a href="/all/artist/style">
-              <i  onClick={handleLogout} style={{color:"#fff"}} className='bx bx-log-out' ></i>
-              <span className="link_name">Cerrar Sesion</span>
-            </a>
-            <ul className="sub-menu blank">
-              <li><a className="link_name" href="/all/artist/style">Cerrar Sesion</a></li>
-            </ul>
-          </li>
-        }
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="#">Inicio</a></li>
+              </ul>
+            </li>
 
 
 
-          <li>
-            <div className="profile-details">
-              <div className="profile-content">
-              { (userData.rol === '[ROLE_USUARIO]') && 
-                <img src={userData.image} alt="" />
-              }
-             
-              { (userData.rol === '[ROLE_ARTISTA]') && 
-                (!userData.PerfilProfesional &&
-                    <img src={userData.PerfilProfesional.img} alt={userData.PerfilProfesional.img} />
-                )
-              }
-             
-              { (userData.rol === '[ROLE_ARTISTA]') && 
-              ( userData.PerfilProfesional.length == 0 &&
-                  <img src={imgnav} alt="..." />
-              )
-              }
-             { (userData.rol === '[ROLE_ARTISTA]') && 
-                  ( userData.PerfilProfesional.length >= 1 &&
-                      <img src={userData.PerfilProfesional[0].img} alt={userData.PerfilProfesional[0].img} />
-                  )
-              }
-              
-              { (userToken == null) && <li></li>
-              }
+          { (userData.rol === '[ROLE_ARTISTA]') &&
+            <li>
+              <div className="iocn-link">
+                <a href="/userTatto/edit-profile">
+                <i className='bx bx-user' ></i>
+                  <span className="link_name">Ver perfil</span>
+                </a>
+                <i className='bx bxs-chevron-down arrow' ></i>
               </div>
+              <ul className="sub-menu">
+                <li><a className="link_name" href="/userTatto/edit-profile">Perfil</a></li>
+                <li><a href="/userTatto/edit-name">Informacion</a></li>
+                <li><a href="/userTatto/edit-quotes">Citas Pendientes</a></li>
+                <li><a href="/userTatto/edit-password">Cambiar Contraseña</a></li>
+              </ul>
+            </li>
+          }
 
- 
-              {(userToken ) && 
-              <div  className="name-job">
-                <div  style={{fontSize: "15px"}}  className="profile_name">
-                {userData.first_name +" "+ userData.last_name }
-                  
-                </div>
-                <div className="job">
-                {userToken  && 
-                   (userData.rol === '[ROLE_USUARIO]') && <div style={{fontSize: "10px"}} className="job">| ROL: USUARIO  |</div>
+          { (userData.rol === '[ROLE_USUARIO]') &&
+            <li>
+              <div className="iocn-link">
+                <a  href="/user/edit-profile">
+                <i className='bx bx-user' ></i>
+                  <span className="link_name">Ver perfil</span>
+                </a>
+                <i className='bx bxs-chevron-down arrow' ></i>
+              </div>
+              <ul className="sub-menu">
+                <li><a className="link_name" href="/user/edit-profile">Perfil</a></li>
+                <li><a href="/user/edit-name">Informacion</a></li>
+                <li><a href="/user/edit-quotes">Citas Pendientes</a></li>
+                <li><a href="/user/edit-image">Cambiar Imagen</a></li>
+
+                <li><a href="/user/edit-password">Cambiar Contraseña</a></li>
+              </ul>
+            </li>
+          }
+
+            <li>
+              <a href="/all/artist/style">
+              <i className='bx bxs-user-account'></i>
+                <span className="link_name">Artistas</span>
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="/all/artist/style">Artistas</a></li>
+              </ul>
+            </li>
+
+
+            <li>
+              <a style={{height:"50px"}} href="/infoTatuajes">
+                <img style={{width:"25px",marginLeft:"28px" }} src={Maquuina} alt={Maquuina} />
+                <span style={{ marginLeft: "1.5rem"}} className="link_name">Tatuajes</span>
+
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="#">Tatuajes</a></li>
+              </ul>
+            </li>
+
+        
+            <li>
+            <a style={{height:"50px"}} href="/infoPiercings">
+                <img style={{width:"25px",marginLeft:"28px" }} src={Pirncings} alt={Maquuina} />
+                <span style={{ marginLeft: "1.5rem"}} className="link_name">Piercings</span>
+
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="#">Piercings</a></li>
+              </ul>
+            </li>
+
+
+            {userToken  && 
+            <li>
+              <a href="/all/artist/style">
+                <i  onClick={handleLogout} style={{color:"#fff"}} className='bx bx-log-out' ></i>
+                <span className="link_name">Cerrar Sesion</span>
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="/all/artist/style">Cerrar Sesion</a></li>
+              </ul>
+            </li>
+          }
+
+
+
+            <li>
+              <div className="profile-details">
+                <div className="profile-content">
+                { (userData.rol === '[ROLE_USUARIO]') && 
+                  <img src={userData.image} alt="" />
                 }
-
-                {userToken  && 
-                 (userData.rol === '[ROLE_ARTISTA]') && <div  style={{fontSize: "10px"}}  className="job">| ROL: ARTISTA  |</div>
+              
+                { (userData.rol === '[ROLE_ARTISTA]') && 
+                  (!userData.PerfilProfesional &&
+                      <img src={userData.PerfilProfesional.img} alt={userData.PerfilProfesional.img} />
+                  )
+                }
+              
+                { (userData.rol === '[ROLE_ARTISTA]') && 
+                ( userData.PerfilProfesional.length == 0 &&
+                    <img src={imgnav} alt="..." />
+                )
+                }
+              { (userData.rol === '[ROLE_ARTISTA]') && 
+                    ( userData.PerfilProfesional.length >= 1 &&
+                        <img src={userData.PerfilProfesional[0].img} alt={userData.PerfilProfesional[0].img} />
+                    )
                 }
                 
+                { (userToken == null) && <li></li>
+                }
                 </div>
-              </div>
-              }
-              
-              { (!userToken == null ) && 
-              <div className="name-job"> </div>
-              }
-            </div>
-            
 
-          </li>
-        </ul>
+  
+                {(userToken ) && 
+                <div  className="name-job">
+                  <div  style={{fontSize: "15px"}}  className="profile_name">
+                  {userData.first_name +" "+ userData.last_name }
+                    
+                  </div>
+                  <div className="job">
+                  {userToken  && 
+                    (userData.rol === '[ROLE_USUARIO]') && <div style={{fontSize: "10px"}} className="job">| ROL: USUARIO  |</div>
+                  }
+
+                  {userToken  && 
+                  (userData.rol === '[ROLE_ARTISTA]') && <div  style={{fontSize: "10px"}}  className="job">| ROL: ARTISTA  |</div>
+                  }
+                  
+                  </div>
+                </div>
+                }
+                
+                { (!userToken == null ) && 
+                <div className="name-job"> </div>
+                }
+              </div>
+              
+
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    }
+
+  {!token && 
+      <div>
+        <div className={`sidebar ${sidebarNav}`} >
+          <div className="logo-details">
+          <div className="home-content">
+            { sidebarNav == "close" &&
+              <i  onClick={HandleClick} style={{cursor:"pointer"}} className='bx bx-menu' ></i>
+            }
+
+            { sidebarNav == "showMenu" &&
+              <i  onClick={HandleClickOpen} style={{cursor:"pointer"}} className='bx bx-menu' ></i>
+            }
+          </div>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href="/">
+                <i className='bx bx-home' ></i>
+                <span className="link_name">Home</span>
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="#">Inicio</a></li>
+              </ul>
+            </li>
+
+
+            <li>
+              <a href="/all/artist/style">
+              <i className='bx bxs-user-account'></i>
+                <span className="link_name">Artistas</span>
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="/all/artist/style">Artistas</a></li>
+              </ul>
+            </li>
+
+
+            <li>
+              <a style={{height:"50px"}} href="/infoTatuajes">
+                <img style={{width:"25px",marginLeft:"28px" }} src={Maquuina} alt={Maquuina} />
+                <span style={{ marginLeft: "1.5rem"}} className="link_name">Tatuajes</span>
+
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="#">Tatuajes</a></li>
+              </ul>
+            </li>
+
+        
+            <li>
+            <a style={{height:"50px"}} href="/infoPiercings">
+                <img style={{width:"25px",marginLeft:"28px" }} src={Pirncings} alt={Maquuina} />
+                <span style={{ marginLeft: "1.5rem"}} className="link_name">Piercings</span>
+
+              </a>
+              <ul className="sub-menu blank">
+                <li><a className="link_name" href="#">Piercings</a></li>
+              </ul>
+            </li>          
+          </ul>
+        </div>
+      </div>
+    }
+
+
+
+    </>
   )
 }
